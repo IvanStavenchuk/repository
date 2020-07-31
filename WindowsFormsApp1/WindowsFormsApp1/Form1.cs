1a7ced7f939bf204c9c0b69ac52be096d6e585d8
IvanStavenchuk <Stavenchuk@gmail.com>
2020-07-31 10:52:06 +0300

    Update Form1.cs

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -15,6 +15,7 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+			int i = 2;
         }
 
         private void Form1_Load(object sender, EventArgs e)
