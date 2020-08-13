28006e3a7f4c3baca1a7f6a4697eded8b1205213
IvanStavenchuk <Stavenchuk@gmail.com>
2020-08-13 15:50:15 +0300

    Test
    IT Pearls

---
+++ ITPearls/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<IPAddress> ips = new List<IPAddress>();
+            foreach (var arg in args)
+            {
+                IPAddress ip;
+                if (IPAddress.TryParse(arg, out ip)) // валидация адресов
+                {
+                    ips.Add(ip);
+                    Console.WriteLine(ip.ToString());
+                }
+            }
+            if (ips.Count == 0)
+                return;
+
+            Console.WriteLine(CalculateMinSubnetIp(ips));
+            Console.ReadKey();
+        }
+
+        public static string CalculateMinSubnetIp(List<IPAddress> ips)
+        {
+            var ipsBytes = ips.Select(x => x.GetAddressBytes());
+            short ipLength = 4;
+            byte[] minIp = new byte[] { 255, 255, 255, 255 };
+            byte[] maxIp = new byte[] { 0, 0, 0, 0 };
+
+            // поиск минимального и максимального адреса
+            for (int i = 0; i < ipLength; i++)
+            {
+                foreach (var ipByte in ipsBytes)
+                {
+                    if (minIp[i] > ipByte[i])
+                        minIp[i] = ipByte[i];
+                    if (maxIp[i] < ipByte[i])
+                        maxIp[i] = ipByte[i];
+                }
+            }
+
+            // вычисление минимального адреса подсети
+            byte[] subnetIp = new byte[ipLength];
+            byte count = 0;
+            int lastOctet = ipLength;
+            for (int i = 0; i < ipLength; i++)
+            {
+                lastOctet--;
+                if (maxIp[i] == minIp[i])
+                {
+                    subnetIp[i] = minIp[i];
+                }
+                else
+                {
+                    count = 1;
+                    while (!((maxIp[i] - minIp[i]) < Math.Pow(2, count)
+                        && minIp[i] > Math.Pow(2, count) * (Math.Truncate(minIp[i] / Math.Pow(2, count)))
+                        && maxIp[i] < Math.Pow(2, count) * (Math.Truncate(minIp[i] / Math.Pow(2, count))+1)
+                        ))
+                    {
+                        count++;
+                    }
+                    subnetIp[i] = (byte)(Math.Pow(2, count) * (Math.Truncate(minIp[i] / Math.Pow(2, count))));
+                    break;
+                }
+            }
+            return $"{subnetIp[0]}.{subnetIp[1]}.{subnetIp[2]}.{subnetIp[3]}/{ 32 - (count + lastOctet * 8)}";
+        }
+    }
+}
---
+++ ITPearls/ConsoleApp1/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,33 @@
+using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Net;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var list = new List<IPAddress>() { IPAddress.Parse("192.168.1.65"), IPAddress.Parse("192.168.1.66") };
+
+            Assert.AreEqual("192.168.1.64/30", Program.CalculateMinSubnetIp(list));            
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var list = new List<IPAddress>() { IPAddress.Parse("192.168.1.17"), IPAddress.Parse("192.168.1.30") };
+
+            Assert.AreEqual("192.168.1.16/28", Program.CalculateMinSubnetIp(list));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var list = new List<IPAddress>() { IPAddress.Parse("192.168.1.17"), IPAddress.Parse("192.168.1.33") };
+
+            Assert.AreEqual("192.168.1.0/26", Program.CalculateMinSubnetIp(list));
+        }
+    }
+}
