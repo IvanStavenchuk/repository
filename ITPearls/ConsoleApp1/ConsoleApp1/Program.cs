10f6f650783cc11abfd6696406f176fd64a14803
IvanStavenchuk <60464875+IvanStavenchuk@users.noreply.github.com>
2020-09-14 20:57:48 +0300

    optimization
    minimize using memory and loops

--- ITPearls/ConsoleApp1/ConsoleApp1/Program.cs
+++ ITPearls/ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,46 +7,38 @@ namespace ConsoleApp1
 {
     public class Program
     {
+        //private static List<byte[]> _ipsBytes = new List<byte[]>();
+        private static short ipLength = 4;
+ 
         static void Main(string[] args)
         {
+            Console.WriteLine(CalculateMinSubnetIp(args));
+            Console.ReadKey();
+        }
 
-
-            List<IPAddress> ips = new List<IPAddress>();
+        public static string CalculateMinSubnetIp(string[] args)
+        {
+            byte[] minIp = new byte[] { 255, 255, 255, 255 };
+            byte[] maxIp = new byte[] { 0, 0, 0, 0 };
+            //IPAddress minIp = new IPAddress(new byte[] { 255, 255, 255, 255 });
+            //IPAddress maxIp = new IPAddress(new byte[] { 0, 0, 0, 0 });
             foreach (var arg in args)
             {
                 IPAddress ip;
                 if (IPAddress.TryParse(arg, out ip)) // валидация адресов
                 {
-                    ips.Add(ip);
+                    byte[] ipByte = ip.GetAddressBytes();
                     Console.WriteLine(ip.ToString());
+                    // поиск минимального и максимального адреса
+                    for (int i = 0; i < ipLength; i++)
+                    {
+                        if (minIp[i] > ipByte[i])
+                            minIp[i] = ipByte[i];
+                        if (maxIp[i] < ipByte[i])
+                            maxIp[i] = ipByte[i];
+                    }
                 }
             }
-            if (ips.Count == 0)
-                return;
-
-            Console.WriteLine(CalculateMinSubnetIp(ips));
-            Console.ReadKey();
-        }
-
-        public static string CalculateMinSubnetIp(List<IPAddress> ips)
-        {
-            var ipsBytes = ips.Select(x => x.GetAddressBytes());
-            short ipLength = 4;
-            byte[] minIp = new byte[] { 255, 255, 255, 255 };
-            byte[] maxIp = new byte[] { 0, 0, 0, 0 };
-
-            // поиск минимального и максимального адреса
-            for (int i = 0; i < ipLength; i++)
-            {
-                foreach (var ipByte in ipsBytes)
-                {
-                    if (minIp[i] > ipByte[i])
-                        minIp[i] = ipByte[i];
-                    if (maxIp[i] < ipByte[i])
-                        maxIp[i] = ipByte[i];
-                }
-            }
-
             // вычисление минимального адреса подсети
             byte[] subnetIp = new byte[ipLength];
             byte count = 0;
--- ITPearls/ConsoleApp1/UnitTestProject/UnitTest1.cs
+++ ITPearls/ConsoleApp1/UnitTestProject/UnitTest1.cs
@@ -11,21 +11,22 @@ namespace UnitTestProject
         [TestMethod]
         public void TestMethod1()
         {
-            var list = new List<IPAddress>() { IPAddress.Parse("192.168.1.65"), IPAddress.Parse("192.168.1.66") };
+            //var list = new List<IPAddress>() { IPAddress.Parse("192.168.1.65"), IPAddress.Parse("192.168.1.66") };
+            var list = new string[] { "192.168.1.65", "192.168.1.66" };
 
             Assert.AreEqual("192.168.1.64/30", Program.CalculateMinSubnetIp(list));            
         }
         [TestMethod]
         public void TestMethod2()
         {
-            var list = new List<IPAddress>() { IPAddress.Parse("192.168.1.17"), IPAddress.Parse("192.168.1.30") };
+            var list = new string[] { "192.168.1.17", "192.168.1.30" };
 
             Assert.AreEqual("192.168.1.16/28", Program.CalculateMinSubnetIp(list));
         }
         [TestMethod]
         public void TestMethod3()
         {
-            var list = new List<IPAddress>() { IPAddress.Parse("192.168.1.17"), IPAddress.Parse("192.168.1.33") };
+        var list = new string[] { "192.168.1.17", "192.168.1.33" };
 
             Assert.AreEqual("192.168.1.0/26", Program.CalculateMinSubnetIp(list));
         }
